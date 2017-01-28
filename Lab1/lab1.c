6f143849c69367e3776ad8e85cde581f9d996296
Robin Andersson <robin182andersson@gmail.com>
2017-01-28 15:44:59 +0100

    Added comments

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -16,10 +16,10 @@ void LCD_Init(void) {
 	LCDFRR = (1 << LCDCD0) | (1 << LCDCD1) | (1 << LCDCD2);
 	// drive time 300 microseconds, contrast control voltage 3.35 V
 	LCDCCR = (1 << LCDCC0) | (1 << LCDCC1) | (1 << LCDCC2) | (1 << LCDCC3);
-	
 }
 
 void writeChar(char ch, int pos) {
+	// Writes a char ch on position pos on the lcd
 	char mask = 0x00;
 	char lcddr = 0xEC;
 	uint16_t SCCTable[10] =  {0x1551, 0x0110, 0x1e11, 0x1B11, 0x0B50, 0x1B41, 0x1F41, 0x0111, 0x1F51, 0x0B51};
@@ -54,76 +54,81 @@ void writeChar(char ch, int pos) {
 		nibble = sccChar & 0x000f;
 		sccChar = sccChar >> 4;
 		
+		// shift nibble left 4 times if pos is uneven because then mask = 0x0F
 		if (pos % 2 != 0) {
 			nibble = nibble << 4;
 		}
 
-		// värde på det lcddr pekar på
+		// use the nibble and the mask to light the parts that nibble says
 		_SFR_MEM8(lcddr) = (_SFR_MEM8(lcddr) & mask) | nibble;		
 		lcddr += 5;
 	}
 }
 
 void writeLong(long i) {
-	int pos = 5;
+	// write the long i on the lcd display, if i contains more than 6 digits the 6 rightmost digits are displayed
+	int pos = 5; // where to display a digit
 	while (i > 0) {
-		int ch = i % 10;
-		writeChar(ch, pos);
-		pos--;
-		i = i / 10;
+		int ch = i % 10; // get the rightmost digit
+		writeChar(ch, pos); // use writeChar to display it
+		pos--; 
+		i = i / 10; // remove rightmost digit
 	}
 }
 
 int is_prime(long i) {
+	// checks if the long i is a prime
 	for (int n = i-1; n >= 2; n--) {
-		if (i % n == 0 && i != n) {
+		if (i % n == 0 && i != n) { // if the number is divisible by something else than itself and 1, return 0
 			return 0;
 		}
 	}
-	return 1;
+	return 1; // if the number passses through the loop without returning 0, then it's a prime
 }
 
 void primes(void) {
+	// generates numbers from 2 and up and checks if the generated number is a prime. if it is then it gets displayed on the lcd
 	long i = 2;
 	while (1) {
 		if (is_prime(i)) {
-			writeLong(i);
+			writeLong(i); // displays the prime on the lcd
 		}
 		i++;
 	}
 }
 
 void blink(void) {
-	TCCR1B = (1 << CS12) | (0 << CS11) | (0 << CS10);
+	// makes a segment of the lcd blink
+	TCCR1B = (1 << CS12) | (0 << CS11) | (0 << CS10); // prescaling factor 256
 	uint16_t timerValue = 0xFFFF / 2;
-	uint8_t check = 0;
+	uint8_t check = 0; // a check which sees when a segment has been turned off/on and if it has passes 0 after doing so
 	
 	while (1) {
 		if (TCNT1 >= timerValue && check == 0) {
-			check == 1;
-			if (LCDDR0 != 0) {
+			check == 1; // set check to 1 to know that we've already fixed the display
+			if (LCDDR0 != 0) { // if the display is on and we've passed timerValue, turn it off
 				LCDDR0 = 0x0;
-			} else {
+			} else { // if the display is off and we've passed timerValue, turn it on
 				LCDDR0 = 0x6;
 			}
 		}
 		
 		if (TCNT1 < timerValue) {
-			check = 0;
+			check = 0; // set check to 0 to know that the cycle has passed
 		}
 	}
 }
 
 void button(void) {
-	PORTB = 0x80;
-	uint8_t previousButton = 0;
+	PORTB = 0x80; // set bit 7 to 1
+	uint8_t previousButton = 0; // remember which button that was used last
 	
 	while (1) {
-		if ( (PINB >> 7) == 1 && previousButton == 0) {
+		if ( (PINB >> 7) == 1 && previousButton == 0) { // if the button is in the middle and last time the button was down
 			LCDDR1 = 0x2;
 			LCDDR2 = 0x0;
 			previousButton = 1;
-		} if ( (PINB >> 7) == 0  && previousButton == 1) {
+		} if ( (PINB >> 7) == 0  && previousButton == 1) { // if the button is down and last time the button was in the middle
 			LCDDR2 = 0x4;
 			LCDDR1 = 0x0;
 			previousButton = 0;
@@ -132,15 +137,16 @@ void button(void) {
 }
 
 void combine(long number) {
+	// combines primes, blink and button into one function
 	PORTB = 0x80;
 	TCCR1B = (1 << CS12) | (0 << CS11) | (0 << CS10);
 	uint16_t timerValue = 0xFFFF / 2;
-	uint8_t previousButton = 0;
-	TCNT1 = 0x0000;
+	uint8_t previousButton = 0; 
 	uint8_t check = 0;
 	
 	while (1) {
 	
+		// button
 		if ( (PINB >> 7) == 1 && previousButton == 0) {
 			LCDDR18 = 0x1;
 			LCDDR13 = 0x0;
@@ -151,10 +157,12 @@ void combine(long number) {
 			previousButton = 0;
 		}
 		
+		// primes
 		if (is_prime(number)) {
 			writeLong(number);
 		}
 		
+		// blink
 		if (TCNT1 >= timerValue && check == 0) {
 			check = 1;
 			if (LCDDR3 != 0) {
@@ -169,7 +177,6 @@ void combine(long number) {
 		}
 		
 		number++;
-		
 	}
 }
 
@@ -190,3 +197,5 @@ int main(void) {
 		combine(2);
     }
 }
+
+// http://pastebin.com/V9cyf7Vb
